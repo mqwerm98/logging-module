e473c7f6be0735c28e46bc4acfaa82a3decc4a26
munzi <mqwerm98@naver.com>
2021-05-24 17:31:25 +0900

    log4j2 설정 및 log pattern 정의

--- munzi-log/src/main/java/log/munzi/interceptor/CustomServletWrappingFilter.java
+++ munzi-log/src/main/java/log/munzi/interceptor/CustomServletWrappingFilter.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
+//@Component
 public class CustomServletWrappingFilter extends OncePerRequestFilter {
 
     @Override
