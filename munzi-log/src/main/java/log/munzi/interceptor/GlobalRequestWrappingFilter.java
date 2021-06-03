2db4f2d2b64e081ab1b81a679075ee91e36c2f1e
munzi <mqwerm98@naver.com>
2021-06-03 10:08:49 +0900

    로깅 모듈 배포에 필요없는 내용 제거 및 security관련 flag 추가
    - log4j2.yml : package-name도 추가로 property로 받아서 처리하도록 변경

--- munzi-log/src/main/java/log/munzi/interceptor/GlobalRequestWrappingFilter.java
+++ munzi-log/src/main/java/log/munzi/interceptor/GlobalRequestWrappingFilter.java
@@ -1,37 +1,27 @@
-package log.munzi.interceptor;
-
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-import org.springframework.web.util.ContentCachingResponseWrapper;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class GlobalRequestWrappingFilter implements Filter {
-
-    @Override
-    public void init(FilterConfig filterConfig) {
-
-    }
-
-    @Override
-    public void destroy() {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest wrappingRequest = new ReadableRequestWrapper((HttpServletRequest) request);
-        ContentCachingResponseWrapper wrappingResponse = new ContentCachingResponseWrapper((HttpServletResponse) response);
-
-        chain.doFilter(wrappingRequest, wrappingResponse);
-
-        wrappingResponse.copyBodyToResponse(); // 캐시를 copy해 return될 response body에 저장
-    }
-
-}
+//package log.munzi.interceptor;
+//
+//import org.springframework.core.Ordered;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.util.ContentCachingResponseWrapper;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@Component
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+//public class GlobalRequestWrappingFilter implements Filter {
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        HttpServletRequest wrappingRequest = new ReadableRequestWrapper((HttpServletRequest) request);
+//        ContentCachingResponseWrapper wrappingResponse = new ContentCachingResponseWrapper((HttpServletResponse) response);
+//
+//        chain.doFilter(wrappingRequest, wrappingResponse);
+//
+//        wrappingResponse.copyBodyToResponse(); // 캐시를 copy해 return될 response body에 저장
+//    }
+//
+//}
