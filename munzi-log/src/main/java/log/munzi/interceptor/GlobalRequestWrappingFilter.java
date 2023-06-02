b4d49814de0756f480dd8c6b06ea1c26fdbb3554
hjpark <hjpark@dkargo.io>
2023-06-02 19:57:45 +0900

    java 17 버전업 작업중..

--- munzi-log/src/main/java/log/munzi/interceptor/GlobalRequestWrappingFilter.java
+++ munzi-log/src/main/java/log/munzi/interceptor/GlobalRequestWrappingFilter.java
@@ -7,9 +7,9 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
--- munzi-log/src/main/java/log/munzi/interceptor/ReadableRequestWrapper.java
+++ munzi-log/src/main/java/log/munzi/interceptor/ReadableRequestWrapper.java
@@ -3,10 +3,10 @@ package log.munzi.interceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 
-import javax.servlet.ReadListener;
-import javax.servlet.ServletInputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.ReadListener;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
